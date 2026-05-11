3b80bf982f971b8fafb0268e0c52afed158ae9bf
Roberivan <roberivansantosilva@gmail.com>
2026-05-11 16:27:42 -0300

    Adicionado mais arquivos

---
+++ src/estrutura_repeticao/estrutura_aninhada_for.java
@@ -0,0 +1,21 @@
+package estrutura_repeticao;
+
+public class estrutura_aninhada_for {
+    static void main() {
+        // Exemplo: Padrão de emoji
+//        for (int linha = 1; linha <= 3; linha++) {
+//            for (int coluna = 1; coluna <= 3; coluna++) {
+//                if (linha == coluna) {
+//                    System.out.print("😊 ");
+//                } else {
+//                    System.out.print("😴 ");
+//                }
+//            }
+//            System.out.println(); // Nova linha
+//        }
+
+        System.out.print("Oi    ");
+        System.out.print("Tudo bem ");
+        System.out.print(", bora lá.");
+    }
+}
---
+++ src/estrutura_repeticao/return_break_continue.java
@@ -0,0 +1,10 @@
+package estrutura_repeticao;
+
+public class return_break_continue {
+    static void main() {
+        for (int i = 0; i < 10; i++) {
+            if (i == 5) break;    // Para o loop
+            if (i == 3) continue; // Pula o 3
+        }
+    }
+}
